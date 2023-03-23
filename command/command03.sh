7f6d026ea1c3e8cd1e783cc2219dbff37bf97a54
rachelse <seamustard52@gmail.com>
2023-03-24 00:38:16 +0900

    updated solutions of modified version

--- command/command03.sh
+++ command/command03.sh
@@ -3,9 +3,9 @@
 #    You can find the structure of GTF file from https://en.wikipedia.org/wiki/General_feature_format
 #    or from the lecture slides.
 grep -v "^#" ~/exercise02/data/d_melanogaster.genes.gtf | awk '{print $1}' | sort | uniq > ~/exercise02/result/result03_1.txt
-# 2. Count genes in each chromosome and find the chromosomes which have 100 or more genes.
-#    Sort the name of chromosomes with 100 or more genes by the default order and write the chromosome names as column 1 and
+# 2. Count genes in each chromosome and find the chromosomes which have 700 or more genes.
+#    Sort the name of chromosomes with 700 or more genes by the default order and write the chromosome names as column 1 and
 #    the count of genes as column 2 to result03_2.csv.
 #    Columns in CSV file, which means Comma-Separated Values, should be separated with comma, ",".
 #    When counting the number of genes in a chromosome, count the lines of which the feature type is "gene".
-grep -v "^#" ~/exercise02/data/d_melanogaster.genes.gtf | awk '{print $1}' | sort | uniq -c | awk '{print $2","$1}' > ~/exercise02/result/result03_2.csv
+grep -v "^#" ~/exercise02/data/d_melanogaster.genes.gtf | awk '{print $1}' | sort | uniq -c | awk 'BEGIN{OFS=","} $1>=700{print $2, $1}' > ~/exercise02/result/result03_2.csv
--- command/command05.sh
+++ command/command05.sh
@@ -1,17 +1,18 @@
 ## command05.sh
-# 1. Download E. coli GTF file and unzip it as "d_melanogaster_old.genes.gtf" in data directory. (No result file)
-# Link: Link: ftp://ftp.ensembl.org/pub//release-89/gtf/drosophila_melanogaster/Drosophila_melanogaster.BDGP6.89.gtf.gz
-wget -O ~/exercise02/data/d_melanogaster_old.genes.gtf.gz "ftp://ftp.ensembl.org/pub//release-89/gtf/drosophila_melanogaster/Drosophila_melanogaster.BDGP6.89.gtf.gz"
-gunzip ~/exercise02/data/d_melanogaster_old.genes.gtf
-# 2. Count the genes in the positive (+) and negative (-) strand of old D. melanogaster genome
+# 1. Download old Drosophila melanogaster GTF file and unzip it as "d_melanogaster_old.genes.gtf" in data directory. (No result file)
+# Link: ftp://ftp.ensembl.org/pub/release-87/gtf/drosophila_melanogaster/Drosophila_melanogaster.BDGP6.87.gtf.gz
+wget -O ~/exercise02/data/d_melanogaster_old.genes.gtf.gz "ftp://ftp.ensembl.org/pub/release-87/gtf/drosophila_melanogaster/Drosophila_melanogaster.BDGP6.87.gtf.gz"
+gunzip ~/exercise02/data/d_melanogaster_old.genes.gtf.gz
+
+# 2. Count the Coding Sequences in the positive (+) and negative (-) strand of old D. melanogaster genome
 #    and save the counts to result05_2.csv.
-grep -v "^#" ./data/d_melanogaster_old.genes.gtf | 
-awk 'BEGIN {OFS=","} {arr[$7]+=1} END {print arr["+"],arr["-"]}' > ~/exercise02/result/result05_2.csv
+grep -v "^#" ~/exercise02/data/d_melanogaster_old.genes.gtf | 
+awk 'BEGIN {OFS=","} $3=="CDS" {arr[$7]+=1} END {print arr["+"],arr["-"]}' > ~/exercise02/result/result05_2.csv
 
 # 3. The gtf file is keeping updated to cover new discoveries.
-#    Let's compare the changes in the number of genomic features (third column in gtf files like exon, gene, CDS, etc).
+#    Let's compare the changes in the number of genomic features (exon, gene, CDS, etc).
 # 
-# 3-1. Count the distinct genomic feature (exon, gene, and others) types of d_melanogaster_old.genes.gtf.
+# 3-1. Count the distinct genomic feature types of d_melanogaster_old.genes.gtf.
 #      Sort the values by default order and write them in result05_3_1.txt following the format of the example of command04-1.
 #
 grep -v "^#" ./data/d_melanogaster_old.genes.gtf | awk '{print $3}' |
@@ -19,7 +20,6 @@ sort | uniq -c | awk '{OFS="\t"}{print $2,$1}' > ~/exercise02/result/result05_3_
 # 3-2. Check join.awk in the command directory. It is from the lecture slide (21th of Linux 2 slides). 
 # You have to make a small change in the code to follow the printing format of the example of command05-3-3.
 # Your changes in join.awk should be pushed.
-#
 
 # 3-3. Join the result05_3_1.txt and result05_3_2.txt using your join.sh and print the result in result05_3_3.txt
 ~/exercise02/command/join.awk ~/exercise02/result/result05_3_1.txt ~/exercise02/result/result04_1.txt > ~/exercise02/result/result05_3_3.txt
